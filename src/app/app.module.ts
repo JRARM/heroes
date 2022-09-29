08411b42f0edfc9377d45a8c83202ca3a0d07dd2
jrarm <rivera.andres6@gmail.com>
2022-09-29 16:40:28 -0500

    1q

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/utils/navbar/navbar.component';
+import { FooterComponent } from './components/utils/footer/footer.component';
+import { CarouselComponent } from './components/utils/carousel/carousel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    FooterComponent,
+    CarouselComponent
   ],
   imports: [
     BrowserModule,
